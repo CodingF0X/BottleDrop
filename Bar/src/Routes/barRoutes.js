17c2b4e4d146cac00699e659936e4b546fc8050f
CodingF0X <roody9a@gmail.com>
2024-07-10 19:49:02 +0200

    GET bar details

--- Bar/src/Routes/barRoutes.js
+++ Bar/src/Routes/barRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createBar } = require('../Controllers/barController');
+const { createBar, getBarDetails, getAllBars } = require('../Controllers/barController');
+const validateObjectId = require('../Middleware/validateObjectId');
 const router = express.Router();
 
 router.post('/', createBar)
-
+router.get('/getAllBars', getAllBars)
+router.get('/:id',validateObjectId, getBarDetails)
 
 module.exports = router;
