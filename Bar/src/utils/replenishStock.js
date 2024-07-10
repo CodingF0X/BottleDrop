9555be802771bf9b161e4c03e3b68f769d16260c
CodingF0X <roody9a@gmail.com>
2024-07-10 20:41:18 +0200

    -- Replenish bar stock

--- Bar/src/utils/replenishStock.js
+++ Bar/src/utils/replenishStock.js
@@ -1,17 +1,17 @@
-const axios = require('axios');
-
-exports.ManuallyUpdateStock = async (barName,bevtype,quantity) => {
- 
-    try {
-      
-        const data = {
-          quantity:quantity,
-          bevtype : bevtype,
-          barName: barName
-        }
-        const response = await axios.post('http://localhost:5000/api/warehouse/replenishBar',data);
-        return response
-    } catch (error) {
-      console.log("Threshold function error: " + error);
-    }
-  }; 
+const axios = require("axios");
+exports.ManuallyUpdateStock = async (barName, bevtype, quantity) => {
+  try {
+    const data = {
+      quantity: quantity,
+      bevtype: bevtype,
+      barName: barName,
+    };
+    const response = await axios.post(
+      "http://localhost:5000/api/warehouse/replenishBar",
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Threshold function error: " + error);
+  }
+};
