03b2cc33eeaac2ff0c13930ad0268dc07d0bd26d
CodingF0X <roody9a@gmail.com>
2024-07-10 18:14:53 +0200

    -- Delete beverage

--- Warehouse/src/Controllers/beverageController.js
+++ Warehouse/src/Controllers/beverageController.js
@@ -1,51 +1,85 @@
 const Beverage = require("../Models/beverageModel");
 const Warehouse = require("../Models/warehouseModel");
 
-exports.createBeverage = async (req,res,next) => {
-    const { beverageType, description, count } = req.body;
+exports.createBeverage = async (req, res, next) => {
+  const { beverageType, description, count } = req.body;
 
-    try {
-        const warehouse = await Warehouse.findOne();
-        const beverages = warehouse.beverages
-        let newBeverages = [];
-        for (let i = 0; i < count; i++) {
-            const newBeverage = await Beverage.create({beverageType, description})
-            newBeverages.push(newBeverage)
-            if(!beverages.has(beverageType)){
-                beverages.set(beverageType,[])
-            }
+  try {
+    const warehouse = await Warehouse.findOne();
+    const beverages = warehouse.beverages;
+    let newBeverages = [];
+    for (let i = 0; i < count; i++) {
+      const newBeverage = await Beverage.create({ beverageType, description });
+      newBeverages.push(newBeverage);
+      if (!beverages.has(beverageType)) {
+        beverages.set(beverageType, []);
+      }
 
-            warehouse.beverages.set(beverageType,[...warehouse.beverages.get(beverageType),newBeverage._id])
-            await warehouse.save();
-        }
-
-        res.status(200).json({ newBeverages, quantity: count });
-
-    } catch (error) {
-        next(error);
+      warehouse.beverages.set(beverageType, [
+        ...warehouse.beverages.get(beverageType),
+        newBeverage._id,
+      ]);
+      await warehouse.save();
     }
-}
 
-exports.updateBeverage = async (req,res,next) => {
-    const { bevType } = req.body;
-    const id = req.params.id;
-    try {
-        
-        const updateBeverage = await Beverage.findByIdAndUpdate(id,{
-            $set: req.body
-        }, {new:true})
+    res.status(200).json({ newBeverages, quantity: count });
+  } catch (error) {
+    next(error);
+  }
+};
 
-        if (!updateBeverage) {
-            return res.status(404).json({ message: 'Beverage not found' });
-        }
+exports.updateBeverage = async (req, res, next) => {
+  const { bevType } = req.body;
+  const id = req.params.id;
+  try {
+    const updateBeverage = await Beverage.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
 
-        const updatedInventory = await Warehouse.updateOne({},{
-            $set:{[`beverages.${bevType}`]: updateBeverage}
-        })
-
-        
-        res.status(200).json(updateBeverage)
-    } catch (error) {
-        next(error)
+    if (!updateBeverage) {
+      return res.status(404).json({ message: "Beverage not found" });
     }
-}
+
+    const updatedInventory = await Warehouse.updateOne(
+      {},
+      {
+        $set: { [`beverages.${bevType}`]: updateBeverage },
+      }
+    );
+
+    res.status(200).json(updateBeverage);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteBeverage = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const deletedBeverage = await Beverage.findByIdAndDelete(id);
+
+    if (!deletedBeverage) {
+      return res.status(404).json({ message: "Beverage not found" });
+    }
+
+    const beverageType = deletedBeverage.beverageType;
+    if (!beverageType) {
+      return res.status(400).json({ message: "Beverage type is undefined" });
+    }
+    const updatedInventory = await Warehouse.updateOne(
+      {},
+      {
+        $pull: {
+          [`beverages.${deletedBeverage.beverageType}`]: deletedBeverage._id,
+        },
+      }
+    );
+    res.status(200).json({ Message: "Beverage has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
--- Warehouse/src/Routes/beverageRoutes.js
+++ Warehouse/src/Routes/beverageRoutes.js
@@ -1,8 +1,10 @@
 const express =  require('express');
-const { createBeverage, updateBeverage } = require('../Controllers/beverageController');
+const { createBeverage, updateBeverage, deleteBeverage } = require('../Controllers/beverageController');
+const validateObjectId = require('../Middleware/validateObjectId');
 const router = express.Router();
 
 router.post('/', createBeverage)
-router.put('/:id', updateBeverage)
+router.put('/:id',validateObjectId, updateBeverage)
+router.delete('/:id',validateObjectId, deleteBeverage)
 
 module.exports = router;
