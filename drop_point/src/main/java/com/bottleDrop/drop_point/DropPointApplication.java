7a4c537f4ddb70c17728c4121754941748a08045
CodingF0X <roody9a@gmail.com>
2024-07-28 19:38:40 +0200

    inital commit

---
+++ drop_point/src/main/java/com/bottleDrop/drop_point/DropPointApplication.java
@@ -0,0 +1,13 @@
+package com.bottleDrop.drop_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropPointApplication.class, args);
+	}
+
+}
---
+++ drop_point/src/test/java/com/bottleDrop/drop_point/DropPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottleDrop.drop_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
