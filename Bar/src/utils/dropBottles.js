9e058eb9f8d8f20696ec7bf5e85e430b933edeae
CodingF0X <roody9a@gmail.com>
2024-08-01 23:25:44 +0200

    fixed the endpoint

--- Bar/src/utils/dropBottles.js
+++ Bar/src/utils/dropBottles.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const collectBottles = async (emptyBottles) =>{
     console.log(emptyBottles)
     try {
-        const response = await axios.put("http://localhost:7000/api/drop_point",{emptyBottles})
+        const response = await axios.post("http://localhost:7000/api/drop_point/dropBottles",{emptyBottles})
         console.log(response.data)
     } catch (error) {
         console.log(error);
